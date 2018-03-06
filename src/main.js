5c2f99240494f80e7e39895e49a82eaca7cf8e68
wuEva <neefoxmo@gmail.com>
2018-03-06 22:32:37 +0800

    挂载了store属性到vue总对象上，保证组件通过this.$store来访问修改store属性方法

--- src/main.js
+++ src/main.js
@@ -3,11 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './assets/js/router'
+import store from './assets/js/vuex/store'
+
+//下面一句配置是去除开发环境下面的多余生产提示
+// You are running Vue in development mode.
+// Make sure to turn on production mode when deploying for production.
+Vue.config.productionTip = false
 
 // 创建和挂载根实例，通过router配置参数注入路由
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
