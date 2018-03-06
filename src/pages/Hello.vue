277d337dfd858ebff0bdfcb7f34a6662bc021294
wuEva <neefoxmo@gmail.com>
2018-03-06 22:36:15 +0800

    修改了错误的参数绑定，由$route.params修改为$route.query

--- src/pages/Hello.vue
+++ src/pages/Hello.vue
@@ -3,7 +3,7 @@
     <p class="alert alert-success">
       由西瓜酱提供包含三种颜色主题的vue后台操作模板
     </p>
-    <p class="alert alert-warning">当前激活面板信息：第{{$route.params.tabIndex}}个面板的第{{$route.params.listIndex}}个子面板</p>
+    <p class="alert alert-warning">当前激活面板信息：第{{$route.query.tabIndex}}个面板的第{{$route.query.listIndex}}个子面板</p>
 
     <p v-for="count in 3" class="alert alert-info" :key="count">{{msg}}</p>
     <p class="alert alert-danger">重要的话要说三遍</p>
