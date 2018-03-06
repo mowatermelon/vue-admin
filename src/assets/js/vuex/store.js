162e2c0ed1e0d8b91a2912ec64866ca091d2c25e
wuEva <neefoxmo@gmail.com>
2018-03-06 22:34:57 +0800

    删除了多余的state数据，和多余修改state数据的方法

--- src/assets/js/vuex/store.js
+++ src/assets/js/vuex/store.js
@@ -1,4 +1,3 @@
-
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -6,13 +5,9 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    update_car: '',
     isLogin: true
   },
   mutations: {
-    getUpdate (state, code) {
-        state.update_car = code
-    },
     checkLogin (state, code) {
         state.isLogin = code
     }
