f58f350ffac46f21193e3afd7d2d2307d914594c
watermelon <neefoxmo@gmail.com>
2018-03-09 13:58:41 +0800

    优化代码缩进和规定添加适当空格

--- src/service/loStorage.js
+++ src/service/loStorage.js
@@ -1,24 +1,25 @@
-function setItem(name,value){
-   localStorage.setItem(name,value);
+'use strict'
+
+function setItem (name, value) {
+  localStorage.setItem(name, value)
 }
 
-function getItem(name,type){
-    let res = localStorage.getItem(name);
-    if(res !== null){
-        if(!type){//为布尔值
-            res = JSON.parse(res);
-        }
+function getItem (name, type) {
+  let res = localStorage.getItem(name)
+  if (res !== null) {
+    if (!type) { // 为布尔值
+      res = JSON.parse(res)
     }
-    return res;
+  }
+  return res
 }
 
-function initData(init){
-
-    for(let i=0;i<init.length;i++){
-        if(localStorage.getItem(init[i].name) === null){
-            setItem(init[i].name,init[i].value);
-        }    
+function initData (init) {
+  for (let i = 0; i < init.length; i++) {
+    if (localStorage.getItem(init[i].name) === null) {
+      setItem(init[i].name, init[i].value)
     }
+  }
 }
 
-export default {initData,setItem,getItem}
+export default {initData, setItem, getItem}
