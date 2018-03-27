8a3b623b0c57079bcfb75125e5accbff8626ffd4
watermelon <neefoxmo@gmail.com>
2018-03-27 18:33:36 +0800

    添加aesDecrypt解密方法，和用户数据解密

--- src/components/header.vue
+++ src/components/header.vue
@@ -27,6 +27,8 @@
 </template>
 
 <script>
+const crypto = require('crypto');
+
 export default {
   name: 'Vheader',
   data () {
@@ -37,8 +39,7 @@ export default {
   },
   computed: {
     uName () {
-      return this.$store.state.userName;
-      
+      return this.aesDecrypt(this.$store.state.userName,'UName');
     }
   },
   methods: {
@@ -58,6 +59,12 @@ export default {
       _this.$store.commit('checkLogin',true);
       // _this.$router.push({path: '/'});
       console.log('loginOut');
+    },
+    aesDecrypt(encrypted, key) {
+        const decipher = crypto.createDecipher('aes192', key);
+        var decrypted = decipher.update(encrypted, 'hex', 'utf8');
+        decrypted += decipher.final('utf8');
+        return decrypted;
     }
   }
 }
