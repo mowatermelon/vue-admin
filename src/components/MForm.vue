b3969c0e22203a68db630753b4629991bc38edd8
watermelon <neefoxmo@gmail.com>
2018-03-27 17:38:28 +0800

    测试新增m-input组件，可以通过 prefix-icon 和 suffix-icon 属性在 input 组件首部和尾部增加显示图标

---
+++ src/components/MForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <m-input v-model="msg" placeholder="请输入内容" :text="msg"></m-input>
+    <m-input v-model="msg1" placeholder="请输入内容1" :text="msg1" type="textarea" textareaStyle="resize:none;"></m-input>
+    <m-input v-model="msg" placeholder="请输入内容2" :text="msg" prefix-icon="headphones"></m-input>
+    <m-input v-model="msg1" placeholder="请输入内容3" :text="msg1" type="textarea" prefix-icon="user"></m-input>
+    <m-input v-model="msg" placeholder="请输入内容4" :text="msg" suffix-icon="heart"></m-input>
+  </div>
+</template>
+
+<script>
+import MInput from '../components/MInput'
+
+export default {
+  name: 'MForm',
+  data () {
+    return {
+      msg:"欢迎查看西瓜的text框",
+      msg1:"欢迎查看西瓜的textarea框"
+    }
+  },
+  components:{
+    MInput
+  },
+  methods:{
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
